6d5bed12366e28731b8a5fb3c2adc8d662e1d79f
kurozael <kurozael@gmail.com>
2022-10-06 15:07:08 +0100

    Apply random force to non-static powerups

--- code/rules/powerpool/PowerupEntity.cs
+++ code/rules/powerpool/PowerupEntity.cs
@@ -7,6 +7,8 @@ namespace Facepunch.Pool
 	{
 		[Net] public Powerup Powerup { get; private set; }
 
+		private TimeUntil NextMovePowerup { get; set; }
+
 		public override void Spawn()
 		{
 			SetModel( "models/pool/pool_ball.vmdl" );
@@ -20,7 +22,32 @@ namespace Facepunch.Pool
 			Powerup = Rand.FromList( powerups ).Create<Powerup>();
 			Powerup.OnSpawn( this );
 
+			EnableTouch = true;
+
 			base.Spawn();
+		}
+
+		public override void StartTouch( Entity other )
+		{
+			if ( other is PoolBall ball && ( ball.Type == PoolBallType.White || ball.Type == Game.Instance.CurrentPlayer.BallType ) )
+			{
+				Log.Info( "Hit powerup: " + Powerup.Name );
+			}
+
+			base.StartTouch( other );
+		}
+
+		[Event.Tick.Server]
+		private void ServerTick()
+		{
+			if ( !Powerup.IsStatic )
+			{
+				if ( NextMovePowerup )
+				{
+					NextMovePowerup = Rand.Float( 4f, 6f );
+					ApplyAbsoluteImpulse( Vector3.Random.WithZ( 0f ) * 100f * 3f );
+				}
+			}
 		}
 	}
 }
