bd583b6dbd274052eb444b66a297170b6a012a5d
kurozael <kurozael@gmail.com>
2022-10-06 15:07:18 +0100

    Add Start, Finish to BaseGameRules and IsPlaying property

--- code/rules/BaseGameRules.cs
+++ code/rules/BaseGameRules.cs
@@ -2,10 +2,44 @@
 
 namespace Facepunch.Pool
 {
-	public abstract class BaseGameRules : BaseNetworkable
+	public abstract partial class BaseGameRules : BaseNetworkable
 	{
+		[Net] public bool IsPlaying { get; set; }
+
 		public virtual bool IsRanked => false;
 		public virtual PoolCue CreatePoolCue() => new PoolCue();
 		public virtual PoolBall CreatePoolBall() => new PoolBall();
+
+		public BaseGameRules()
+		{
+			Event.Register( this );
+		}
+
+		~BaseGameRules()
+		{
+			Event.Unregister( this );
+		}
+
+		public void Start()
+		{
+			IsPlaying = true;
+			OnStart();
+		}
+
+		public void Finish()
+		{
+			IsPlaying = false;
+			OnFinish();
+		}
+
+		protected virtual void OnStart()
+		{
+
+		}
+
+		protected virtual void OnFinish()
+		{
+
+		}
 	}
 }
