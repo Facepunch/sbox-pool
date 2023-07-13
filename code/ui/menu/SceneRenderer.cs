bd61e374f86c25f18744e1955b81da3f48db8388
kurozael <kurozael@gmail.com>
2023-07-13 15:54:21 +0100

    Scene renderer for pool map

---
+++ code/ui/menu/SceneRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+using Sandbox.Menu;
+using System;
+using Sandbox.UI;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Facepunch.Pool.Menu;
+
+public class SceneRenderer : ScenePanel
+{
+	private SceneMap Map { get; set; }
+
+	public SceneRenderer()
+	{
+		World = new SceneWorld();
+		Map = new SceneMap( World, "maps/pool_lounge_v2" );
+
+		Camera.BackgroundColor = Color.Black;
+		Camera.FieldOfView = 60f;
+		Camera.Position = Vector3.Up * 100f;
+		Camera.AmbientLightColor = Color.Black;
+		Camera.Rotation = Rotation.LookAt( Vector3.Down );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+	}
+}
